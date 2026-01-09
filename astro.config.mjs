25b59b6cae4afc8d0fcec4139229148da90a2a75
Nick Taylor <nick@nickyt.co>
2026-01-09 07:21:18 -0500

    feat: added React astro integation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,24 +2,31 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import varlockintegration from "@varlock/astro-integration";
+import { ENV } from "varlock/env";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: netlify(),
   site: "https://www.nickyt.co",
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
   markdown: {
     shikiConfig: {
       theme: "github-dark",
     },
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), varlockintegration()],
     resolve: {
       alias: {
         "@": "/src",
       },
     },
+  },
+  server: {
+    port: ENV.PORT,
   },
 });
