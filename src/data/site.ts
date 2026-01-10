24ffb429960bc80d5932d4131fdb49d3d799da1b
Nick Taylor <nick@nickyt.co>
2026-01-10 18:51:59 -0500

    refactor: remove unused properties from SiteConfig and update mastodonHandle

--- src/data/site.ts
+++ src/data/site.ts
@@ -3,37 +3,27 @@ const getSiteUrl = (_context = process.env.CONTEXT) => {
 };
 
 export interface SiteConfig {
-  showThemeCredit: boolean;
   name: string;
   shortDesc: string;
   url: string;
-  authorEmail: string;
   twitterHandle: string;
   mastodonHandle: string;
   authorName: string;
-  enableThirdPartyComments: boolean;
   maxPostsPerPage: number;
   paymentPointer: string;
   faviconPath: string;
-  newsletterName: string;
-  newsletterRss: string;
 }
 
 export const site: SiteConfig = {
-  showThemeCredit: true,
   name: "Just Some Dev",
   shortDesc: "Welcome to Nick Taylor's personal website.",
   url: getSiteUrl(),
-  authorEmail: "nick@nickyt.co",
   twitterHandle: "@nickytonline",
-  mastodonHandle: "@nickytonline@toot.cafe",
+  mastodonHandle: "@nickytonline@hachyderm.io",
   authorName: "Nick Taylor",
-  enableThirdPartyComments: false,
   maxPostsPerPage: 5,
   paymentPointer: "$ilp.uphold.com/MZMhAWA7bkGa",
   faviconPath: "/images/favicon.svg",
-  newsletterName: "Yet Another Newsletter LOL",
-  newsletterRss: "https://buttondown.email/nickytonline/rss",
 };
 
 export default site;
